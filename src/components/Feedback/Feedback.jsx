00cbec3c84db0ecd130d63ec521b047535559030
A-yadav07 <ayadav02072001@gmail.com>
2024-12-02 19:11:18 +0530

    seve

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react"; // Import Swiper components
 import "swiper/css"; // Import Swiper styles
 import "swiper/css/pagination";
