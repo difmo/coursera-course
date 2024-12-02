416c3310f0a64e672f416ebfaa6310a2924bd0e1
amarcodeservir <amarcodeservir@gmail.com>
2024-12-02 11:09:57 +0530

    save

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      Home
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+        reprehenderit inventore architecto id quis animi, laboriosam ipsum
+        ratione hic esse consequuntur. Alias omnis, ratione iste minima quos
+        ipsam assumenda perferendis.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
