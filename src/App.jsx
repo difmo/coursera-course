dc961944dad69ec92db6f03635470a30a01d9868
amarcodeservir <amarcodeservir@gmail.com>
2024-12-03 18:06:03 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./components/Layout/Layout";
 import Onlinetraining from "./components/MedicalCoding/Onlinetraining";
-import MedicalCoding from "./components/ReadMorePages/Medicalcoding";
-import Australiancoding from "./components/ReadMorePages/Australiancoding";
-import Medicalbilling from "./components/ReadMorePages/Medicalbilling";
-import Medicalcalling from "./components/ReadMorePages/Medicalcalling";
 import ClassroomTrain from "./components/MedicalCoding/ClassroomTrain";
 
 function App() {
@@ -18,12 +14,6 @@ function App() {
           <Route path="Onlinetraining" element={<Onlinetraining />} />
           <Route path="ClassroomTrain" element={<ClassroomTrain />} />
           <Route path="ClassroomTrain" element={<ClassroomTrain />} />
-          {/*  */}
-          <Route path="MedicalCoding" element={<MedicalCoding />} />
-          <Route path="Australiancoding" element={<Australiancoding />} />
-          <Route path="Medicalbilling" element={<Medicalbilling />} />
-          <Route path="Medicalcalling" element={<Medicalcalling />} />
-          {/*  */}
         </Route>
       </Routes>
     </Router>
